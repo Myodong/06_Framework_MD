de43aba1082cf0b6e3dab7581a2d7c6efedfcfdf
Myodong <gyehd1230@naver.com>
2022-11-17 17:37:04 +0900

    .

--- project2/src/main/java/edu/kh/project/board/model/vo/Board.java
+++ project2/src/main/java/edu/kh/project/board/model/vo/Board.java
@@ -8,5 +8,17 @@ import lombok.ToString;
 @Setter
 @Getter
 public class Board {
+	private int boardNo;
+	private String boardTitle;
+	private String boardContent;
+	private String boardCreateDate;
+	private String boardUpdateDate;
+	private int readCount;
+	private int commentCount;
+	private int likeCount;
+	private String memberNickname;
+	private int memberNo;
+	private String profileImage;
+	private String thumbnail;
 
 }
